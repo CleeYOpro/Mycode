ea01c1485a5812f576a35520cecaba149c963f73
Cleofus Balaranjith <152673306+CleeYOpro@users.noreply.github.com>
2024-02-12 19:51:17 -0800

    Added Solution - GfG to GitHub

---
+++ School/Remove characters from alphanumeric string/remove-characters-from-alphanumeric-string.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+	def removeCharacters(self, S):
+	    s = ""
+		for i in S:
+		    if i.isnumeric():
+		        s+=i
+
+		return s
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+		
+		ob = Solution()	
+		answer = ob.removeCharacters(s)
+		
+		print(answer)
+
+
+# } Driver Code Ends
